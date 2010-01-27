1c7624fd09b64eafe0276b9731f1be3db1dbb213
Fantomas42 <fantomas42@gmail.com>
2010-01-27 17:49:49 +0100

    packaging app

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='django-smileys',
+      version=version,
+      
+      description='A usefull and incredible Django application that allow you to use smileys in your templates :)',
+      long_description=open(os.path.join('README.rst')).read(),
+      keywords='django, smiley',
+
+      author='Fantomas42',
+      author_email='fantomas42@gmail.com',
+      url='http://github.com/Fantomas42/django-smileys',
+
+      packages=['smileys'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',],
+            
+      license='BSD License',
+      include_package_data=True,
+      zip_safe=True,)
+
+
