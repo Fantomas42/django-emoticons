0099c2d74bcd2cfe4ef2565bb3fc2abb445411c3
Fantomas42 <fantomas42@gmail.com>
2013-10-29 08:25:12 +0100

    Refactor template tag and filte

--- emoticons/templatetags/emoticon_tags.py
+++ emoticons/templatetags/emoticon_tags.py
@@ -12,37 +12,51 @@ from emoticons.settings import EMOTICONS_CLASS
 
 register = template.Library()
 
-RE_EMOTICONS_LIST = [(re.compile(re.escape(emoticon[0])),
-                      emoticon[0], emoticon[1])
-                     for emoticon in EMOTICONS_LIST]
+RE_EMOTICONS_LIST = [
+    (re.compile(re.escape(emoticon[0])), emoticon[0], emoticon[1])
+    for emoticon in EMOTICONS_LIST
+]
 
 
-def replace_emoticons(content, autoescape=None):
-    esc = autoescape and conditional_escape or (lambda x: x)
-    content = esc(content)
+def replace_emoticons(content):
+    """
+    Replace the emoticons string by HTML images.
+    """
     for emoticon, name, image in RE_EMOTICONS_LIST:
         if emoticon.search(content):
             emoticon_html = '<img class="%s" src="%s" alt="%s" />' % (
                 EMOTICONS_CLASS, os.path.join(EMOTICONS_URL, image), name)
             content = emoticon.sub(emoticon_html, content)
-    return mark_safe(content)
-replace_emoticons.needs_autoescape = True
+    return content
 
 
-class SmileyNode(template.Node):
+class EmoticonNode(template.Node):
+    """
+    Node for applying ``replace_emoticons`` on content.
+    """
     def __init__(self, nodelist):
         self.nodelist = nodelist
 
     def render(self, context):
-        output = self.nodelist.render(context)
-        return replace_emoticons(output)
+        content = self.nodelist.render(context)
+        return replace_emoticons(content)
 
 
-def do_replace_emoticons(parser, token):
-    nodelist = parser.parse(('endemoticons',))
+@register.tag('emoticons')
+def emoticons_tag(parser, token):
+    """
+    Tag for rendering emoticons.
+    """
+    nodelist = parser.parse(['endemoticons'])
     parser.delete_first_token()
-    return SmileyNode(nodelist)
+    return EmoticonNode(nodelist)
 
 
-register.tag('emoticons', do_replace_emoticons)
-register.filter('emoticons', replace_emoticons)
+@register.filter('emoticons', needs_autoescape=True)
+def emoticons_filter(content, autoescape=None):
+    """
+    Filter for rendering emoticons.
+    """
+    esc = autoescape and conditional_escape or (lambda x: x)
+    content = mark_safe(replace_emoticons(esc(content)))
+    return content
