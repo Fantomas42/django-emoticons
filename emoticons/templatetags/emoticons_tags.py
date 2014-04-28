7472411cbebe1bfb24678d52045f946b372f47d5
Fantomas42 <fantomas42@gmail.com>
2014-04-28 20:53:19 +0200

    Update the API of the tag and the filter to handle the excluded markups

--- emoticons/templatetags/emoticons_tags.py
+++ emoticons/templatetags/emoticons_tags.py
@@ -9,7 +9,7 @@ from emoticons.settings import EMOTICONS_COMPILED
 register = template.Library()
 
 
-def replace_emoticons(content):
+def replace_emoticons(content, excluded_markups):
     """
     Replace the emoticons string by HTML images.
     """
@@ -23,12 +23,13 @@ class EmoticonNode(template.Node):
     """
     Node for applying ``replace_emoticons`` on content.
     """
-    def __init__(self, nodelist):
+    def __init__(self, nodelist, exclude):
         self.nodelist = nodelist
+        self.exclude = exclude
 
     def render(self, context):
         content = self.nodelist.render(context)
-        return replace_emoticons(content)
+        return replace_emoticons(content, self.exclude)
 
 
 @register.tag('emoticons')
@@ -36,18 +37,26 @@ def emoticons_tag(parser, token):
     """
     Tag for rendering emoticons.
     """
+    exclude = ''
+    args = token.split_contents()
+    if len(args) == 2:
+        exclude = args[1]
+    elif len(args) > 2:
+        raise template.TemplateSyntaxError(
+            'emoticons tag has only one optional argument')
+
     nodelist = parser.parse(['endemoticons'])
     parser.delete_first_token()
-    return EmoticonNode(nodelist)
+    return EmoticonNode(nodelist, exclude)
 
 
 @register.filter('emoticons', needs_autoescape=True)
-def emoticons_filter(content, autoescape=None):
+def emoticons_filter(content, exclude='', autoescape=None):
     """
     Filter for rendering emoticons.
     """
     esc = autoescape and conditional_escape or (lambda x: x)
-    content = mark_safe(replace_emoticons(esc(content)))
+    content = mark_safe(replace_emoticons(esc(content), exclude))
     return content
 
 
