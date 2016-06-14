641642e030f1482cba37e0f51b75db219266dd3a
Fantomas42 <fantomas42@gmail.com>
2016-06-14 16:03:26 +0200

    Update the demo

--- emoticons/demo/settings.py
+++ emoticons/demo/settings.py
@@ -4,7 +4,6 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 STATIC_URL = '/static/'
 
@@ -12,14 +11,13 @@ SECRET_KEY = 'secret-key'
 
 ROOT_URLCONF = 'emoticons.demo.urls'
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_DIRS = (
-    PROJECT_ROOT,
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [PROJECT_ROOT],
+        'APP_DIRS': True,
+    }
+]
 
 INSTALLED_APPS = (
     'emoticons',
